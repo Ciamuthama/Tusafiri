178ae8a539d890f34aafd9a5d345eec874a2a8b2
Ciamuthama <muthamapeter79@gmail.com>
2023-08-21 17:36:56 +0300

    added more context to the welcome screen(gradient and responsive width and height)

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -5,15 +5,18 @@ import HomeScreen from '../screens/homeScreen';
 import WelcomeScreen from '../screens/welcomeScreen';
 
 
+
 const Stack = createNativeStackNavigator();
 
 function AppNavigation() {
     return (
         <NavigationContainer>
+            
             <Stack.Navigator initialRouteName='Welcome' screenOptions={{headerShown:false}} >
-                {/* <Stack.Screen name='Home' component={HomeScreen} /> */}
-                <Stack.Screen name='Welcome Screen' component={WelcomeScreen} options={{ headerShow: false }} />
+                <Stack.Screen name='Home' component={HomeScreen} />
+                <Stack.Screen name='Welcome Screen' component={WelcomeScreen} />
             </Stack.Navigator>
+      
         </NavigationContainer>
     );
 }
