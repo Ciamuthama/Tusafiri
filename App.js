9ded3d07dde155c40c4a61c4aad5d55153e1c3b6
Peter Muthama <114055846+Ciamuthama@users.noreply.github.com>
2024-02-05 15:33:18 +0300

    Update App.js  status bar

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import AppNavigation from './src/navigations';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
 
   return (
+    <>
     <AppNavigation />
+     <StatusBar style="auto" />
+    </>
   );
 }
 
